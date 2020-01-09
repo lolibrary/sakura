8dba6b7bef85a5897f134c85759bfa685cd07b7c
Amelia Ikeda <amelia@lolibrary.org>
2020-01-09 13:24:50 +0000

    Fix default image

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -150,7 +150,7 @@ if (! function_exists('default_asset')) {
      */
     function default_asset()
     {
-        return Storage::url('images/default.jpeg');
+        return Storage::url('assets/default.png');
     }
 }
 
