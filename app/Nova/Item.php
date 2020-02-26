fab9503018ab4f0e88a227fc97ca8c27172f61cc
Cocoa <cocoa@lolibrary.org>
2020-02-26 12:39:19 -0500

    Add actions for pending items

--- app/Nova/Item.php
+++ app/Nova/Item.php
@@ -3,7 +3,7 @@
 namespace App\Nova;
 
 use App\Models\Item as BaseItem;
-use App\Nova\Actions\{PublishItem, UnpublishItem};
+use App\Nova\Actions\{PublishItem, UnpublishItem, PendingItem};
 use App\Nova\Filters\ItemStatusFilter;
 use Laravel\Nova\Panel;
 use Laravel\Nova\Fields\ID;
@@ -243,6 +243,15 @@ class Item extends Resource
                 }
 
                 return $model->published() && $request->user()->can('publish', $model);
+            }),
+                (new PendingItem)->canSee(function ($request) {
+                $model = $request->findModelQuery()->first();
+
+                if ($model === null) {
+                    return $request->user()->lolibrarian();
+                }
+
+                return $model->draft() && $request->user()->can('update', $model);
             }),
         ];
     }
