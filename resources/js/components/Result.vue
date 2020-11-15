d76b01d553597c2e1fd205c45f06200d077aff4b
Cocoa <momijizukamori@gmail.com>
2020-11-15 17:39:37 -0500

    cleanup debugging

--- resources/js/components/Result.vue
+++ resources/js/components/Result.vue
@@ -40,7 +40,6 @@
     <a class="btn btn-outline-primary rounded-0" style="border: none;" :href="item.url">
         View Item
     </a>
-    {{ user.level }}
     <a v-if="user.level >= 100" class=" btn btn-outline-primary rounded-0" style="border: none;" :href="item.edit_url">
         Edit Item
     </a>
