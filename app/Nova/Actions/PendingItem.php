5f162e4745a1ff2e66e13aaa051be426949d0d8c
momijizukamori <momijizukamori+bugzilla@gmail.com>
2021-02-11 20:22:49 -0500

    Add action to mark items pending (#14)

---
+++ app/Nova/Actions/PendingItem.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Request;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+
+class PendingItem extends Action
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        $models->each->setPending();
+
+        return Action::message('Marked pending!');
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return [];
+    }
+
+    /**
+     * Check an item is authorized to run.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Item $model
+     * @return bool
+     */
+    public function authorizedToRun(Request $request, $model)
+    {
+        if ($model->published()) {
+            return false;
+        }
+
+        return $request->user()->can('update', $model);
+    }
+}
