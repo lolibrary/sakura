7b0276fa7f4acfd4fee8325fb99b411e9ad9c43a
Cocoa <momijizukamori@gmail.com>
2021-05-08 20:04:58 -0400

    Fix migration name

--- database/migrations/0054____multiple_categories_items.php
+++ database/migrations/0054____multiple_categories_items.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class MultipleItemCategories extends Migration
+class MultipleCategoriesItems extends Migration
 {
     /**
      * Run the migrations.
