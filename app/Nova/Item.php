73d043e92340197305bc438dd7a27dc3bff49d40
Cocoa <momijizukamori@gmail.com>
2021-05-08 22:14:10 -0400

    remove broken migration

--- app/Nova/Item.php
+++ app/Nova/Item.php
@@ -115,7 +115,7 @@ class Item extends Resource
                 ->hideFromIndex(),
 
             BelongsTo::make('Brand')->sortable(),
-            AttachMany::make('Category', 'categories', Category::class)->rules('min:1'),
+            AttachMany::make('Category', 'categories', Category::class)->rules('min:1', 'required'),
 
             Trix::make('Notes')->alwaysShow(),
             Trix::make('Internal Notes')->alwaysShow(),
