b6045a1d202cce985a8d1004435553bad299949b
Cocoa <momijizukamori@gmail.com>
2022-09-23 19:23:52 -0400

    Numeric check on price field

--- app/Nova/Item.php
+++ app/Nova/Item.php
@@ -136,6 +136,7 @@ class Item extends Resource
                     ->displayUsingLabels()
                     ->hideFromIndex(),
                 Text::make('Price')
+                    ->rules('numeric'),
                     ->hideFromIndex(),
             ]),
 
