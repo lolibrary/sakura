194122f08b9c9190f44d7b5188b1f5bf848d6d47
Cocoa <momijizukamori@gmail.com>
2023-06-13 21:19:07 -0400

    debugging

--- app/Http/Middleware/Localize.php
+++ app/Http/Middleware/Localize.php
@@ -15,6 +15,7 @@ class Localize
     public function handle($request, Closure $next)
     {
         $lang = session('lang');
+        $lang = 'en';
         // TODO: add logic for checking if they have a stored value for language once that's implemented
         if ($lang) {
             App::setLocale($lang);
