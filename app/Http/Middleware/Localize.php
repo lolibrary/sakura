ae31c38978f4df6b25ddd8834db89315de52438e
momijizukamori <momijizukamori>
2023-06-14 11:46:54 -0400

    more debugging

--- app/Http/Middleware/Localize.php
+++ app/Http/Middleware/Localize.php
@@ -15,12 +15,13 @@ class Localize
      */
     public function handle($request, Closure $next)
     {
-        $lang = session('lang');
+        $lang = $request->session('lang');
         $lang = 'en';
         // TODO: add logic for checking if they have a stored value for language once that's implemented
         if ($lang) {
             App::setLocale($lang);
         }
+        $request->session()->flash('status', $lang);
         return $next($request);
     }
 }
