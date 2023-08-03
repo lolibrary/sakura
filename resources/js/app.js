e265fb7925d1ca87695c93eda12584c30fa5bf16
momijizukamori <momijizukamori>
2023-08-03 15:27:39 -0400

    Remove Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,20 +7,20 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+// /**
+//  * Next, we will create a fresh Vue application instance and attach it to
+//  * the page. Then, you may begin adding components to this application
+//  * or customize the JavaScript scaffolding to fit your unique needs.
+//  */
 
-Vue.component('v-pagination', require('./components/Pagination.vue').default);
-Vue.component('search-page', require('./components/Search.vue').default);
-Vue.component('v-select', require('vue-select').VueSelect);
-Vue.component('b-form-radio-group', require('bootstrap-vue').BFormRadioGroup);
-Vue.component('search-result', require('./components/Result.vue').default);
+// Vue.component('v-pagination', require('./components/Pagination.vue').default);
+// Vue.component('search-page', require('./components/Search.vue').default);
+// Vue.component('v-select', require('vue-select').VueSelect);
+// Vue.component('b-form-radio-group', require('bootstrap-vue').BFormRadioGroup);
+// Vue.component('search-result', require('./components/Result.vue').default);
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
