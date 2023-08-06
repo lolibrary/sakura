2ab682234ea813404a757f275c6d2cf3b71db01a
M <maria.p.sjoberg+weblate@gmail.com>
2023-08-06 21:39:18 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: Lolibrary/Validation
    Translate-URL: https://hosted.weblate.org/projects/lolibrary/validation/sv/

--- resources/lang/sv/validation.php
+++ resources/lang/sv/validation.php
@@ -10,22 +10,22 @@ return [
     | as the size rules. Feel free to tweak each of these messages here.
     |
     */
-    'confirmed' => '',
-    'email' => '',
+    'confirmed' => 'Bekräftelsen för :attribute stämmer inte överens.',
+    'email' => ':attribute måste vara en giltig epostadress.',
     'max' => [
-        'string' => '',
+        'string' => ':attribute får inte vara längre än :max karaktärer.',
     ],
     'min' => [
         // 'numeric' => 'The :attribute may not be greater than :max.',
         // 'file' => 'The :attribute may not be greater than :max kilobytes.',
         // 'array' => 'The :attribute may not have more than :max items.',
-        'string' => '',
+        'string' => ':attribute måste innehålla minst :min karaktärer.',
     ],
     // 'numeric' => 'The :attribute must be at least :min.',
     // 'file' => 'The :attribute must be at least :min kilobytes.',
     // 'array' => 'The :attribute must have at least :min items.',
-    'regex' => '',
-    'required' => '',
-    'string' => '',
-    'unique' => '',
+    'regex' => 'Formatet på :attribute är ogiltigt.',
+    'required' => 'Fältet för :attribute är obligatoriskt.',
+    'string' => ':attribute måste vara en sträng.',
+    'unique' => ':attribute är redan registrerat.',
 ];
