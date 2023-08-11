7e9285ab4a05a27e6f3e345df5acba9915702e3e
momijizukamori <momijizukamori>
2023-08-11 14:51:52 -0400

    Fix config syntax

--- config/app.php
+++ config/app.php
@@ -110,7 +110,7 @@ return [
     'faker_locale' => 'en_US',
 
     // Available locales
-    'locales' => ['en' => 'English', 'fr' => 'français', 'nb_NO' => 'norsk (bokmål)']
+    'locales' => ['en' => 'English', 'fr' => 'français', 'nb_NO' => 'norsk (bokmål)'],
 
     /*
     |--------------------------------------------------------------------------
