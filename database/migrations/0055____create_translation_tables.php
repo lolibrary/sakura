1e7685148fc8a752744dfb4f7751733f88b695f3
momijizukamori <momijizukamori>
2023-08-15 11:30:54 -0400

    Fix migrations

--- database/migrations/0055____create_translation_tables.php
+++ database/migrations/0055____create_translation_tables.php
@@ -30,22 +30,23 @@ class CreateTranslationTables extends Migration
          * - drop the name column from the existing model table
          */
 
-         $creator = function($table) use ($single, $plural) {
-            $table->increments('id');
-            $table->uuid("{$single}_id");
-            $table->string('locale')->index();
-            $table->string('name');
-            $table->timestampsTz();
-          
-            $table->unique(["{$single}_id", 'locale']);
-            $table->foreign("{$single}_id")->references('id')->on($plural)->onDelete('cascade');
-        };
+         foreach(TABLES as $single => $plural) {
+            $creator = function($table) use ($single, $plural) {
+                $table->increments('id');
+                $table->uuid("{$single}_id");
+                $table->string('locale')->index();
+                $table->string('name');
+                $table->timestampsTz();
+            
+                $table->unique(["{$single}_id", 'locale']);
+                $table->foreign("{$single}_id")->references('id')->on($plural)->onDelete('cascade');
+            };
 
-        $migrator = function($table) use ($single, $plural) {
-            DB::statement("insert into ? (?, name, locale) select id, name, 'en' from ?", ["${single}_translations", "${single}_id", $plural]);
-        };
+            $migrator = function($table) use ($single, $plural) {
+                DB::insert("insert into ${single}_translations (${single}_id, name, locale) select id, name, 'en' from $plural");
+            };
 
-        foreach(TABLES as $single => $plural) {
+
             Schema::create("${single}_translations", $creator); 
             Schema::table("${single}_translations", $migrator);
     
