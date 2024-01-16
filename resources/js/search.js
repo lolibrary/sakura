f96a9545ac689c4d93c227de55ff4526f2357792
Diana Chan <diana@sleepycow.org>
2024-01-15 19:22:23 -0800

    Rewritten to use object literal namespacing

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,115 +1,117 @@
 const token = document.head.querySelector('meta[name="csrf-token"]');
 
 $(() => {
-    let year_slider = $("#year-slider").slider();
+    const searchJs = {
+        headers: {
+            "X-CSRF-TOKEN": token.content,
+            "X-Requested-With": "XMLHttpRequest",
+        },
+        init: () => {
+            searchJs.year_slider = $("#year-slider").slider();
+            searchJs.loader = $('#search-results-loading');
+            searchJs.results = $('#search-results');
+            searchJs.year_slider.on('slideStop', (evt) => {
+                searchJs.doSearch();
+            });
 
-    year_slider.on('slideStop', (evt) => {
-       doSearch();
-    });
+            $('.form-control-chosen').chosen().on('change', (evt) => {
+                let filter = $(evt.currentTarget);
+                searchJs.matchVisibility(filter);
+                searchJs.doSearch();
+            });
 
-    $('.form-control-chosen').chosen().on('change', (evt) => {
-        let filter = $(evt.currentTarget);
-        matchVisibility(filter);
-        doSearch();
-    })
+            $('button[name="action:search"]')
+            .on('click', (evt) => {
+                searchJs.triggerSearch(evt)
+            });
 
-    $('button[name="action:search"]')
-    .on('click', (evt) => {
-        triggerSearch(evt)
-    })
+            $('.match_type input:radio, .year_match_type input:radio')
+            .on('click', (evt) => {
+                $(evt.currentTarget).parent().parent().find('label').removeClass('active');
+                $(evt.currentTarget).parent().addClass('active');
+                searchJs.doSearch();
+            })
 
-    $('.match_type input:radio, .year_match_type input:radio')
-        .on('click', (evt) => {
-            $(evt.currentTarget).parent().parent().find('label').removeClass('active');
-            $(evt.currentTarget).parent().addClass('active');
-            doSearch();
-        })
 
-    $('#search').on('keypress', (evt) => {
-        if (evt.which == 13) {
-            triggerSearch(evt)
-        }
-    })
+            $('#search').on('keypress', (evt) => {
+                if (evt.which == 13) {
+                    searchJs.triggerSearch(evt)
+                }
+            });
 
-    $('button[name="action:clear"]').on('click', (evt) => {
-        evt.stopPropagation();
-        evt.preventDefault();
+            $('button[name="action:clear"]').on('click', (evt) => {
+                evt.stopPropagation();
+                evt.preventDefault();
 
-        $('select.form-control-filter').val([]);
-        $('select.form-control-filter').trigger('chosen:updated');
-        $('.match_type').hide();
-        year_slider.slider('setValue', [1970, 2023]);
+                $('select.form-control-filter').val([]);
+                $('select.form-control-filter').trigger('chosen:updated');
+                $('.match_type').hide();
+                searchJs.year_slider.slider('setValue', [1970, 2023]);
 
-        let any = $('.match-any');
-        any.find('input').prop('checked', true);
-        any.addClass('active');
+                let any = $('.match-any');
+                any.find('input').prop('checked', true);
+                any.addClass('active');
 
-        let all_none = $('.match-all, .match-none');
-        all_none.find('input').prop('checked', false);
-        all_none.removeClass('active');
-        doSearch();
-    })
+                let all_none = $('.match-all, .match-none');
+                all_none.find('input').prop('checked', false);
+                all_none.removeClass('active');
+                searchJs.doSearch();
+            });
 
-    $('select.form-control-filter').each(function(){matchVisibility($(this))});
-});
+            $('select.form-control-filter').each(function(){searchJs.matchVisibility($(this))});
+        },
 
-const headers = {
-    "X-CSRF-TOKEN": token.content,
-    "X-Requested-With": "XMLHttpRequest",
-  };
-
-const results = $('#search-results');
-const loader = $('#search-results-loading');
-
-function matchVisibility(filter) {
-    let matcher = filter.nextAll('.match_type');
-    if (filter.val().length > 0) {
-        matcher.show();
-    } else {
-        matcher.hide();
-    }
-}
-
-function triggerSearch(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    doSearch();
-}
-
-function getFormValues() {
-    let form_values = $('#search-form').serializeArray();
-    let exclude_matching = ['search'];
-    let filter_names = form_values.map((form_obj) => form_obj.name.replace('[]', ''));
-    return form_values.filter((form_obj) => {
-        let filter_name = form_obj.name.toLowerCase();
-        if (filter_name === 'search' && form_obj.value == '') {
-            return false;
-        }
-        if (exclude_matching.includes(filter_name)) {
-            return true;
-        }
-        let match_index = filter_name.search('matcher');
-        if (match_index !== -1) {
-            let base_filter = filter_name.substr(0, match_index - 1);
-            if (!filter_names.includes(base_filter)) {
-                return false;
+        triggerSearch: (evt) => {
+            evt.stopPropagation();
+            evt.preventDefault();
+            searchJs.doSearch();
+        },
+        doSearch: () => {
+            let form = document.getElementById('search-form');
+            let form_values = searchJs.getFormValues();
+            searchJs.loader.css('display', 'block');
+            searchJs.results.css('display', 'none');
+            window.history.pushState(null, null, '/search/?' + $.param(form_values));
+            fetch('/search', { method: "POST", headers: searchJs.headers, body: new FormData(form)})
+                .then((response) => response.text())
+                .then((text) => {
+                    searchJs.loader.css('display', 'none');
+                    searchJs.results.css('display', 'block');
+                    searchJs.results.html(text);
+                });
+        },
+        matchVisibility: (filter) => {
+            let matcher = filter.nextAll('.match_type');
+            if (filter.val().length > 0) {
+                matcher.show();
+            } else {
+                matcher.hide();
             }
-        }
-        return true;
-    });
-}
+        },
 
-function doSearch() {
-    let form = document.getElementById('search-form');
-    let form_values = getFormValues();
-    loader.css('display', 'block');
-    results.css('display', 'none');
-    window.history.pushState(null, null, '/search/?' + $.param(form_values));
-    fetch('/search', { method: "POST", headers: headers, body: new FormData(form)})
-        .then((response) => response.text())
-        .then((text) => {
-            loader.css('display', 'none');
-            results.css('display', 'block');
-            results.html(text);
-        });
-  }
+        getFormValues: () => {
+            let form_values = $('#search-form').serializeArray();
+            let exclude_matching = ['search'];
+            let filter_names = form_values.map((form_obj) => form_obj.name.replace('[]', ''));
+            return form_values.filter((form_obj) => {
+                let filter_name = form_obj.name.toLowerCase();
+                if (filter_name === 'search' && form_obj.value == '') {
+                    return false;
+                }
+                if (exclude_matching.includes(filter_name)) {
+                    return true;
+                }
+                let match_index = filter_name.search('matcher');
+                if (match_index !== -1) {
+                    let base_filter = filter_name.substr(0, match_index - 1);
+                    if (!filter_names.includes(base_filter)) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+    }
+
+    searchJs.init();
+});
