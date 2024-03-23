f3ed6aef8091df8a34a23493d71348f7e7c1a211
Cocoa <momijizukamori@gmail.com>
2024-03-23 00:08:16 -0400

    Clean up vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import react from '@vitejs/plugin-react';
 import vue from '@vitejs/plugin-vue2';
 
 export default defineConfig({
@@ -9,7 +8,6 @@ export default defineConfig({
             'resources/sass/app.scss',
             'resources/js/app.js',
         ]),
-        // react(),
         vue({
             template: {
                 transformAssetUrls: {
