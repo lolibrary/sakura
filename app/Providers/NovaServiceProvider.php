ff2a5f3d370308a5edade272b83e052d13a68321
momijizukamori <momijizukamori>
2025-01-10 18:03:18 -0500

    Try using public_path

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -27,7 +27,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
         parent::boot();
 
-        Nova::style("css-overrides", asset("assets/overrides.css"));
+        Nova::style("css-overrides", public_path("assets/overrides.css"));
     }
 
     /**
