2f4e58130700249d1b3085a3fbc25f27b0da90d7
தமிழ்நேரம் <anishprabu.t@gmail.com>
2025-01-13 00:34:56 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: Lolibrary/Validation
    Translate-URL: https://hosted.weblate.org/projects/lolibrary/validation/ta/

--- resources/lang/ta/validation.php
+++ resources/lang/ta/validation.php
@@ -10,24 +10,24 @@ return [
     | as the size rules. Feel free to tweak each of these messages here.
     |
     */
-    'array' => '',
-    'confirmed' => '',
-    'email' => '',
-    'integer' => '',
+    'array' => 'அந்த :attribute ஒரு வரிசையாக இருக்க வேண்டும்.',
+    'confirmed' => 'அந்த :attribute உறுதிப்படுத்தல் பொருந்தவில்லை.',
+    'email' => 'அந்த :attribute சரியான மின்னஞ்சல் முகவரியாக இருக்க வேண்டும்.',
+    'integer' => 'அந்த :attribute ஒரு முழு எண்ணாக இருக்க வேண்டும்.',
     'max' => [
-        'string' => '',
+        'string' => 'அந்த :attribute :max எழுத்துக்கள் விட அதிகமாக இருக்காது.',
     ],
     'min' => [
         // 'numeric' => 'The :attribute may not be greater than :max.',
         // 'file' => 'The :attribute may not be greater than :max kilobytes.',
         // 'array' => 'The :attribute may not have more than :max items.',
-        'string' => '',
-        'numeric' => '',
+        'string' => 'அந்த :attribute குறைந்தபட்சம் :min எழுத்துக்கள் இருக்க வேண்டும்.',
+        'numeric' => 'அந்த :attribute குறைந்தபட்சம் :min இருக்க வேண்டும்.',
     ],
     // 'file' => 'The :attribute must be at least :min kilobytes.',
     // 'array' => 'The :attribute must have at least :min items.',
-    'regex' => '',
-    'required' => '',
-    'string' => '',
-    'unique' => '',
+    'regex' => 'அந்த :attribute வடிவம் தவறானது.',
+    'required' => 'அந்த :attribute புலம் தேவை.',
+    'string' => 'அந்த :attribute ஒரு சரமாக இருக்க வேண்டும்.',
+    'unique' => 'அந்த :attribute ஏற்கனவே எடுக்கப்பட்டுள்ளது.',
 ];
