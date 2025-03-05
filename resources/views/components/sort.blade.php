364784a644fd272a5aafe3193859b4b56445f6df
momijizukamori <momijizukamori>
2025-03-05 14:23:17 -0500

    clean up sort code slightly?

--- resources/views/components/sort.blade.php
+++ resources/views/components/sort.blade.php
@@ -1,11 +1,10 @@
 <div class="card">
     <div class="card-header">
-      {{__('ui.sort.title')}}
+        <label class="control-label" for="sort">{{__('ui.sort.title')}}</label>
     </div>
     <div class="card-body">
         <div class="pb-2">
-            <label class="control-label" for="sort">{{ __('ui.sort.title')}}</label>
-            <select style="width: 100%" name="sort" id="sort" data-placeholder="Tap to sort" class="form-control form-control-chosen form-control-filter">
+            <select style="width: 100%" name="sort" id="sort" data-placeholder="Tap to sort" class="form-control form-control-filter">
               @foreach($sorts as $sort)
               <option value ="{{$sort}}" @if(in_array($sort, request($sort, []) )) selected @endif> {{ __('ui.sort.' . $sort)}}</option>
               @endforeach
