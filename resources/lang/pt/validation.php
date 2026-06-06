da5506897308a28f10aae2f24e0a1f489ac8fdfc
ssantos <ssantos@web.de>
2026-06-06 11:27:33 +0200

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: Lolibrary/Validation
    Translate-URL: https://hosted.weblate.org/projects/lolibrary/validation/pt/

--- resources/lang/pt/validation.php
+++ resources/lang/pt/validation.php
@@ -10,24 +10,24 @@ return [
     | as the size rules. Feel free to tweak each of these messages here.
     |
     */
-    'array' => '',
-    'confirmed' => '',
-    'email' => '',
-    'integer' => '',
+    'array' => 'O :attribute deve ser um array.',
+    'confirmed' => 'A confirmação de :attribute não corresponde.',
+    'email' => 'O :attribute deve ser um email válido.',
+    'integer' => 'O :attribute deve ser um número inteiro.',
     'max' => [
-        'string' => '',
+        'string' => 'O :attribute já não deve ter que :max caracteres.',
     ],
     'min' => [
         // 'numeric' => 'The :attribute may not be greater than :max.',
         // 'file' => 'The :attribute may not be greater than :max kilobytes.',
         // 'array' => 'The :attribute may not have more than :max items.',
-        'string' => '',
-        'numeric' => '',
+        'string' => 'O :attribute deve ter pelo menos :min caracteres.',
+        'numeric' => 'O :attribute deve ter pelo menos :min.',
     ],
     // 'file' => 'The :attribute must be at least :min kilobytes.',
     // 'array' => 'The :attribute must have at least :min items.',
-    'regex' => '',
-    'required' => '',
-    'string' => '',
-    'unique' => '',
+    'regex' => 'O formato de :attribute é inválido.',
+    'required' => 'O campo :attribute é obrigatório.',
+    'string' => 'O :attribute deve ser uma cadeia.',
+    'unique' => 'O :attribute já foi utilizado.',
 ];
