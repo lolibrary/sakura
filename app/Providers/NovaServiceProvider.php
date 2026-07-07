3c6ac2d2f22a46d62b8825143712fa197cc8538e
Amelia Wylie <52548998+MellieVT@users.noreply.github.com>
2026-07-07 02:36:43 +0100

    major: sort out the majority of laravel 10's upgrade, minus nova

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Nova\Dashboards\Main;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Nova;
@@ -81,7 +82,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
      */
     protected function dashboards()
     {
-        return [];
+        return [
+            new Main,
+        ];
     }
 
     /**
--- config/nova.php
+++ config/nova.php
@@ -4,6 +4,7 @@ use Laravel\Nova\Http\Middleware\Authenticate;
 use Laravel\Nova\Http\Middleware\Authorize;
 use Laravel\Nova\Http\Middleware\BootTools;
 use Laravel\Nova\Http\Middleware\DispatchServingNovaEvent;
+use Laravel\Nova\Http\Middleware\HandleInertiaRequests;
 
 return [
 
@@ -72,9 +73,14 @@ return [
 
     'middleware' => [
         'web',
-        Authenticate::class,
+        HandleInertiaRequests::class,
         DispatchServingNovaEvent::class,
         BootTools::class,
+    ],
+
+    'api_middleware' => [
+        'nova',
+        Authenticate::class,
         Authorize::class,
     ],
 
@@ -90,5 +96,7 @@ return [
     */
 
     'pagination' => 'simple',
+
+    'storage_disk' => env('NOVA_STORAGE_DISK', env('FILESYSTEM_CLOUD', 'public')),
 
 ];
