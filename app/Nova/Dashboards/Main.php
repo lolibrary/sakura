ecc833930558e50191d1521024a912e946bafd6c
Amelia Wylie <52548998+MellieVT@users.noreply.github.com>
2026-07-24 19:31:37 +0100

    fix: my -> your submissions

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -3,7 +3,7 @@
 namespace App\Nova\Dashboards;
 
 use App\Nova\Metrics\ItemsPublished;
-use App\Nova\Metrics\MySubmissions;
+use App\Nova\Metrics\YourSubmissions;
 use Laravel\Nova\Dashboards\Main as Dashboard;
 
 class Main extends Dashboard
@@ -19,7 +19,7 @@ class Main extends Dashboard
             ItemsPublished::make()
                 ->width('1/4')
                 ->help('Items published on Lolibrary to date.'),
-            MySubmissions::make()
+            YourSubmissions::make()
                 ->width('1/4')
                 ->help('All items you have created which are in the "draft", "pending" or "changes requested" states.'),
         ];
