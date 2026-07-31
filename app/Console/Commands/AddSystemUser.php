cf1698443f8bf9affd3d23062c588ea523116806
Amelia Wylie <52548998+MellieVT@users.noreply.github.com>
2026-07-31 14:26:30 +0100

    add the ability to generate system users

---
+++ app/Console/Commands/AddSystemUser.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\Level;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+use Illuminate\Support\Str;
+use function Laravel\Prompts\error;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\table;
+
+class AddSystemUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:system-user {username} {--email=} {--level=1000}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Adds a system user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $username = $this->argument('username');
+        $email = $this->option('email') ?? "admin+$username@lolibrary.org";
+        $level = Level::tryFrom((int)$this->option('level'));
+
+        if (User::where('username', $username)->exists()) {
+            error('A user already exists with that username');
+            return;
+        }
+
+        if (User::where('email', $email)->exists()) {
+            error('A user already exists with that email');
+            return;
+        }
+
+        if ($level === null) {
+            error('Invalid level, please try again');
+            return;
+        }
+
+        $user = new User;
+
+        $user->forceFill([
+            'name' => $username,
+            'username' => $username,
+            'email' => $email,
+            'level' => $level,
+            'password' => bcrypt(Str::random(64)),
+            'email_verified_at' => now(),
+        ]);
+
+        $user->save();
+
+        info("Created user");
+        table(['key', 'value'], collect([
+            'id' => $user->getKey(),
+            'email' => $user->email,
+            'username' => $user->username,
+            'level' => $user->level->getLabel(),
+            'verified' => 'true',
+        ])->map(fn (string $value, string $key) => compact('key', 'value'))
+            ->all());
+    }
+}
