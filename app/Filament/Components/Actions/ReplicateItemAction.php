fc54e6d978caab8b39f0f00d35c4dcc2321a16e0
Amelia Wylie <52548998+MellieVT@users.noreply.github.com>
2026-08-01 01:58:12 +0100

    stick menu items for entries in an actiongroup + change colours

---
+++ app/Filament/Components/Actions/ReplicateItemAction.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Components\Actions;
+
+use App\Models\Attribute;
+use App\Models\Item;
+use Filament\Actions\ReplicateAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Support\Icons\Heroicon;
+use Illuminate\Support\Facades\DB;
+
+class ReplicateItemAction
+{
+    public static function make(): ReplicateAction
+    {
+        return ReplicateAction::make()
+            ->icon(Heroicon::OutlinedClipboardDocument)
+            ->color('gray')
+            ->schema([
+                TextInput::make('english_name')
+                    ->maxLength(255)
+                    ->required()
+            ])
+            ->excludeAttributes([
+                'image',
+                'images',
+                'slug',
+                'publisher_id',
+                'user_id',
+                'published_at',
+                'status',
+                'created_at',
+                'updated_at',
+            ])
+            ->mutateRecordDataUsing(function (array $data): array {
+                $data['user_id'] = auth()->id();
+
+                return $data;
+            })
+            ->after(function (Item $record, Item $replica): void {
+                DB::transaction(function () use ($record, $replica) {
+                    $replica->categories()->sync($record->categories);
+                    $replica->features()->sync($record->features);
+                    $replica->colors()->sync($record->colors);
+                    $replica->tags()->sync($record->tags);
+                    $replica->attributes()->sync(
+                        $record->attributes
+                            ->mapWithKeys(fn(Attribute $attr) => [
+                                $attr->id => [
+                                    'value' => $attr->value,
+                                ],
+                            ])
+                    );
+                });
+
+
+            })
+            ->successRedirectUrl(fn(Item $replica) => $replica->view_url);
+    }
+}
--- app/Filament/Resources/Items/Pages/ViewItem.php
+++ app/Filament/Resources/Items/Pages/ViewItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Items\Pages;
 
+use App\Filament\Components\Actions\ReplicateItemAction;
 use App\Filament\Resources\Items\ItemResource;
 use App\Jobs\MarkAsDraft;
 use App\Jobs\PublishItem;
@@ -11,6 +12,7 @@ use App\Jobs\UnpublishItem;
 use App\Models\Attribute;
 use App\Models\Item;
 use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
 use Filament\Actions\EditAction;
 use Filament\Actions\ReplicateAction;
 use Filament\Forms\Components\TextInput;
@@ -27,76 +29,41 @@ class ViewItem extends ViewRecord
         return [
             EditAction::make()
                 ->icon(Heroicon::OutlinedPencilSquare),
-            ReplicateAction::make()
-                ->icon(Heroicon::OutlinedClipboardDocument)
-                ->color('gray')
-                ->schema([
-                    TextInput::make('english_name')
-                        ->maxLength(255)
-                        ->required()
-                ])
-                ->excludeAttributes([
-                    'image',
-                    'images',
-                    'slug',
-                    'publisher_id',
-                    'user_id',
-                    'published_at',
-                    'status',
-                    'created_at',
-                    'updated_at',
-                ])
-                ->mutateRecordDataUsing(function (array $data): array {
-                    $data['user_id'] = auth()->id();
 
-                    return $data;
-                })
-                ->after(function (Item $record, Item $replica): void {
-                    DB::transaction(function () use ($record, $replica) {
-                        $replica->categories()->sync($record->categories);
-                        $replica->features()->sync($record->features);
-                        $replica->colors()->sync($record->colors);
-                        $replica->tags()->sync($record->tags);
-                        $replica->attributes()->sync(
-                            $record->attributes
-                                ->mapWithKeys(fn(Attribute $attr) => [
-                                    $attr->id => [
-                                        'value' => $attr->value,
-                                    ],
-                                ])
-                        );
-                    });
-
-
-                })
-                ->successRedirectUrl(fn(Item $replica) => $replica->view_url),
-            Action::make('publish')
-                ->requiresConfirmation()
-                ->icon(Heroicon::OutlinedCheckBadge)
-                ->color('success')
-                ->authorize('publish')
-                ->action(fn() => dispatch(new PublishItem($this->record, auth()->user())) && $this->fillForm()),
-            Action::make('unpublish')
-                ->requiresConfirmation()
-                ->icon(Heroicon::OutlinedArchiveBoxXMark)
-                ->color('danger')
-                ->authorize('unpublish')
-                ->action(fn() => dispatch(new UnpublishItem($this->record, auth()->user())) && $this->fillForm()),
             Action::make('ready_for_review')
                 ->icon(Heroicon::OutlinedClipboardDocumentCheck)
-                ->color('info')
+                ->color('light')
                 ->authorize('readyForReview')
+                ->tooltip('Flag this entry to senior volunteers to check over')
                 ->action(fn() => dispatch(new ReadyForReview($this->record, auth()->user())) && $this->fillForm()),
             Action::make('mark_as_draft')
                 ->icon(Heroicon::OutlinedDocument)
                 ->color('gray')
                 ->authorize('markAsDraft')
+                ->tooltip('Mark this entry as no longer ready for review')
                 ->action(fn() => dispatch(new MarkAsDraft($this->record, auth()->user())) && $this->fillForm()),
             Action::make('request_changes')
                 ->icon(Heroicon::OutlinedChatBubbleLeftEllipsis)
                 ->color('warning')
                 ->authorize('requestChanges')
                 ->action(fn() => dispatch(new RequestChanges($this->record, auth()->user())) && $this->fillForm()),
+
+            ActionGroup::make([
+                ReplicateItemAction::make()
+                    ->color('fuschia')
+                    ->tooltip('Copies an item, minus the images, with a new name'),
+                Action::make('unpublish')
+                    ->icon(Heroicon::OutlinedArchiveBoxXMark)
+                    ->color('danger')
+                    ->authorize('unpublish')
+                    ->tooltip('Remove an entry from the main lolibrary.org site')
+                    ->action(fn() => dispatch(new UnpublishItem($this->record, auth()->user())) && $this->fillForm()),
+                Action::make('publish')
+                    ->icon(Heroicon::OutlinedCheckBadge)
+                    ->color('success')
+                    ->authorize('publish')
+                    ->action(fn() => dispatch(new PublishItem($this->record, auth()->user())) && $this->fillForm()),
+            ]),
         ];
     }
 }
