7e26198ac1789f6a6c06f25cd8c8adacf2c1c419
Amelia Wylie <52548998+MellieVT@users.noreply.github.com>
2026-08-02 12:39:00 +0100

    308 redirect to duplicate URLs if available

--- app/Http/Controllers/Items/ItemController.php
+++ app/Http/Controllers/Items/ItemController.php
@@ -4,23 +4,9 @@ namespace App\Http\Controllers\Items;
 
 use App\Enums\Status;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\ItemStoreRequest;
-use App\Http\Requests\Admin\ItemUpdateRequest;
-use App\Models\Attribute;
-use App\Models\Brand;
-use App\Models\Category;
-use App\Models\Color;
-use App\Models\Feature;
-use App\Models\Image;
 use App\Models\Item;
-use App\Models\Tag;
-use App\Models\User;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\App;
 
 class ItemController extends Controller
 {
@@ -28,15 +14,19 @@ class ItemController extends Controller
      * Show an item.
      *
      * @param \App\Models\Item $item
-     * @return \Illuminate\Http\Response|\Illuminate\View\View
+     * @return RedirectResponse|\Illuminate\View\View
      */
     public function show(Item $item)
     {
-        $item->load(Item::FULLY_LOAD);
+        if ($item->status === Status::Duplicate && $item->duplicate_url !== null) {
+            return redirect($item->duplicate_url, status: 308); // permanent redirect.
+        }
 
         if ($item->status !== Status::Published && auth()->guest()) {
             abort(404);
         }
+
+        $item->load(Item::FULLY_LOAD);
 
         return view('items.show', compact('item'));
     }
