85eab64d0db424d0e9d99fa8b9031a7ee561b74b
Amelia Wylie <52548998+MellieVT@users.noreply.github.com>
2026-08-14 18:22:17 +0100

    lowercase emails before doing any auth operation

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +28,21 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Get the password reset credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        $data = $request->only('password', 'password_confirmation', 'token');
+
+        if (! is_null($email = $request->input('email'))) {
+            $data['email'] = str($email)->lower()->toString();
+        }
+
+        return $data;
+    }
 }
